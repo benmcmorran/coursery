ab3300e0b7a3554b9aebbd3e4bbac1de16526fa4
Ben McMorran <ben.mcmorran@gmail.com>
2014-05-07 17:26:30 -0400

    Adds old site and scraper sources from my HDD

---
+++ src/scraper/BannerWebCrawler/BannerWebCrawler/CookieAwareWebClient.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BannerWebCrawler
+{
+    public class CookieAwareWebClient : WebClient
+    {
+        public bool UpdateCookies { get; set; }
+
+        public CookieAwareWebClient(CookieContainer container)
+        {
+            this.container = container;
+            UpdateCookies = true;
+        }
+
+        private readonly CookieContainer container = new CookieContainer();
+
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            WebRequest r = base.GetWebRequest(address);
+            r.Timeout = 300000;
+            var request = r as HttpWebRequest;
+            if (request != null)
+            {
+                request.CookieContainer = container;
+            }
+            return r;
+        }
+
+        protected override WebResponse GetWebResponse(WebRequest request, IAsyncResult result)
+        {
+            WebResponse response = base.GetWebResponse(request, result);
+            ReadCookies(response);
+            return response;
+        }
+
+        protected override WebResponse GetWebResponse(WebRequest request)
+        {
+            WebResponse response = base.GetWebResponse(request);
+            ReadCookies(response);
+            return response;
+        }
+
+        private void ReadCookies(WebResponse r)
+        {
+            var response = r as HttpWebResponse;
+            if (response != null)
+            {
+                CookieCollection cookies = response.Cookies;
+                foreach (Cookie cookie in cookies)
+                {
+                    container.Add(new Uri("https://" + cookie.Domain), cookie);
+                }
+            }
+        }
+    }
+}
---
+++ src/scraper/BannerWebCrawler/BannerWebCrawler/Evaluation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BannerWebCrawler
+{
+    class Evaluation
+    {
+        public int Year { get; set; }
+        public string Subject { get; set; }
+        public string Course { get; set; }
+        public string Section { get; set; }
+        public string Instructor { get; set; }
+        public double CourseQuality { get; set; }
+        public double InstructorQuality { get; set; }
+        public int ClassTime { get; set; }
+        public string Grade { get; set; }
+
+        public string ToJS()
+        {
+            return String.Format("{{yr:{0},su:\"{1}\",cr:\"{2}\",sc:\"{3}\",in:\"{4}\",rc:{5:F1},ri:{6:F1},wl:{7},gr:\"{8}\"}}",
+                    Year, Subject, Course, Section, Instructor, CourseQuality, InstructorQuality, ClassTime, Grade);
+        }
+
+        public string ToFile()
+        {
+            return String.Format("{0}-{1}-{2}-{3}.html", Year, Subject, Course, Section);
+        }
+    }
+}
